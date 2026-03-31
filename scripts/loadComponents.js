ccbcc306259c263f0ae2a2a99562d23070d7700e
magneticEagle <156685650+magneticEagle@users.noreply.github.com>
2026-03-30 20:56:54 -0400

    Footer and Header logic updated

--- scripts/loadComponents.js
+++ scripts/loadComponents.js
@@ -1,5 +1,10 @@
-fetch('/components/header.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('header').innerHTML = data;
-  });
+document.querySelectorAll("[data-component]").forEach(el => {
+  const file = el.getAttribute("data-component");
+
+  fetch(file)
+    .then(res => res.text())
+    .then(data => {
+      el.innerHTML = data;
+    })
+    .catch(err => console.error(`Error loading ${file}:`, err));
+});
