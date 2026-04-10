e6a51f4be1e54aa125af32e4fa142c1f878ec9b2
Joey Battista <joeybattista@Joeys-MacBook-Air-4.local>
2026-04-10 19:02:53 -0400

    Added Spinner loading icons to page navigation

--- scripts/loadComponents.js
+++ scripts/loadComponents.js
@@ -5,6 +5,48 @@ document.querySelectorAll("[data-component]").forEach(el => {
     .then(res => res.text())
     .then(data => {
       el.innerHTML = data;
+
+      // Header spinner for navigation
+      if (file.includes("header")) {
+        const pageSpinner = document.getElementById("page-spinner");
+
+        document.querySelectorAll("a").forEach(link => {
+          link.addEventListener("click", function(e) {
+            const href = link.getAttribute("href");
+            if (
+              link.hostname === window.location.hostname &&
+              href &&
+              !href.startsWith("#")
+            ) {
+              pageSpinner.classList.remove("hidden");
+            }
+          });
+        });
+
+        window.addEventListener("pageshow", () => {
+          setTimeout(() => {
+            pageSpinner.classList.add("hidden");
+          }, 1500);
+        });
+      }
+
+      // Footer spinner for navigation
+      if (file.includes("footer")) {
+        const pageSpinner = document.getElementById("page-spinner");
+
+        el.querySelectorAll("a").forEach(link => {
+          link.addEventListener("click", function(e) {
+            const href = link.getAttribute("href");
+            if (
+              link.hostname === window.location.hostname &&
+              href &&
+              !href.startsWith("#")
+            ) {
+              pageSpinner.classList.remove("hidden");
+            }
+          });
+        });
+      }
     })
     .catch(err => console.error(`Error loading ${file}:`, err));
 });
