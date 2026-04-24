3f32450b27cbf13d0cd79594faa43c4c607a770d
Carson <138606571+carsoncarmody@users.noreply.github.com>
2026-04-24 15:49:28 -0400

    Update click event to handle page spinner visibility
    Delay showing the page spinner until after the click event is processed.

--- scripts/loadComponents.js
+++ scripts/loadComponents.js
@@ -29,7 +29,7 @@ document.querySelectorAll("[data-component]").forEach((el) => {
         const pageSpinner = document.getElementById("page-spinner");
 
         document.querySelectorAll("a").forEach((link) => {
-          link.addEventListener("click", function () {
+          link.addEventListener("click", function (event) {
             const href = link.getAttribute("href");
 
             if (
@@ -37,7 +37,12 @@ document.querySelectorAll("[data-component]").forEach((el) => {
               href &&
               !href.startsWith("#")
             ) {
-              pageSpinner.classList.remove("hidden");
+              
+              setTimeout(function () {
+                if (!event.defaultPrevented) {
+                  pageSpinner.classList.remove("hidden");
+                }
+              }, 0);
             }
           });
         });
